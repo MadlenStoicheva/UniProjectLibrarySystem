a6609f1594f9da9ae8413f121182285340b1a4c4
MadlenStoicheva <infinite.love@abv.bg>
2018-03-20 14:25:18 +0200

    Change authenticationFilter

--- LibrarySystem.Web/ActionFilters/AuthenticationFilter.cs
+++ LibrarySystem.Web/ActionFilters/AuthenticationFilter.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace LibrarySystem.Web.ActionFilters
 {
@@ -18,21 +19,39 @@ namespace LibrarySystem.Web.ActionFilters
             RequireAdminRole = false;
         }
 
+        //public override void OnActionExecuting(ActionExecutingContext filterContext)
+        //{
+        //    User user = (User)HttpContext.Current.Session["LoggedUser"];
+
+        //    if (user == null)
+        //    {
+        //        filterContext.Result = new RedirectResult("/Home/IndexPage");
+        //        return;
+        //    }
+
+        //    if (RequireAdminRole == true && user.IsAdmin != true)
+        //    {
+        //        filterContext.Result = new RedirectResult("/Home/IndexPage");
+        //        return;
+        //    }
+        //}
+
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             User user = (User)HttpContext.Current.Session["LoggedUser"];
 
             if (user == null)
             {
-                filterContext.Result = new RedirectResult("/Home/IndexPage");
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Home", action = "IndexPage" }));
                 return;
             }
-
+            base.OnActionExecuting(filterContext);
             if (RequireAdminRole == true && user.IsAdmin != true)
             {
-                filterContext.Result = new RedirectResult("/Home/IndexPage");
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Home", action = "IndexPage" }));
                 return;
             }
+            base.OnActionExecuting(filterContext);
         }
     }
 }
